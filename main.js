b8940c4a3e08250932767ff6f4e08ab7d6a0638a
LSC <lelandscrawford@gmail.com>
2020-08-17 16:42:03 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+
+/* These are the codewars problems I did:
+https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/solutions/javascript
+
+function removeChar(str) {
+  return str.slice(1, -1);
+}
+
+
+
+
+
+
+*/
